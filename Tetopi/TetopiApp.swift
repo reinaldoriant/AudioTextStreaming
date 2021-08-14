0abb26da84e4bde2f000b7d9a2168a6f17acdf89
Reinaldo Riant Kurnia Perdana <reinaldoriant@gmail.com>
2021-08-14 17:00:06 +0700

    Initial Commit

---
+++ Tetopi/TetopiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TetopiApp.swift
+//  Tetopi
+//
+//  Created by TI Digital on 14/08/21.
+//
+
+import SwiftUI
+
+@main
+struct TetopiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
