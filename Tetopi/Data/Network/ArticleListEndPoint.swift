3a55238d98debb2771da57b7b37d6ccdda85b4dd
Reinaldo Riant Kurnia Perdana <reinaldoriant@gmail.com>
2021-08-18 00:05:53 +0700

    ! for General

--- Tetopi/Data/Network/ArticleListEndPoint.swift
+++ Tetopi/Data/Network/ArticleListEndPoint.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-var TOKEN_BEARER = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9hcGlnZW4ua29tcGFzLmlkIiwiaWF0IjoxNjI5MjE5NzEzLCJleHAiOjE2MjkyMjA2MTMsImRhdGEiOnsiaWQiOiI0N2ExNzUwOC00OWE3LTRiNjUtYjM0Mi0zNDEwZTVmMzRhODciLCJlbWFpbCI6InJlaW5hbGRvLnJpYW50QGtvbXBhcy5jb20iLCJzY29wZSI6WyJyZWFkLWFydGljbGUiLCJyZWFkLXByb2ZpbGUiXX19.WcOsN5_ehnAZ6Rvm4PiQQtIT2iIXJhsy4KYA7JkpfyTPUc-rQxsIfqxpE9FIlsaiQTOmRpXKZINycSR3X1fkZPTEnmKzDBzYgFXY4-dgyeNJ0d8PtZ5UWM9SJTYbewqDQ1br4HLrQe6HYM3Cx3quh9ybsiJs8HG0K6cGt5mS4Ak"
+var TOKEN_BEARER = "YOUR TOKEN"
 //MARK: - Enum Search API
 enum ArticleListAPI {
     case getTerbaruListNoCursor (siteId : String, timeStamp : String)
@@ -32,14 +32,14 @@ extension ArticleListAPI: APIBuilder {
     var baseUrl: URL {
         switch self {
         case .getTerbaruListNoCursor :
-            return URL(string: "https://apikid.kompas.id/")!
+            return URL(string: "YOUR BASE URL")!
         }
     }
     
     var path: String {
         switch self {
         case .getTerbaruListNoCursor :
-            return "v2/article/list/terms"
+            return "YOUR PATH"
         }
     }
 }
